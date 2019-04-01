4a81889f8cddd8d940656c47560df6a765bf01bd
HokoriXIII <13hokori@gmail.com>
2019-04-01 15:54:43 +0300

    module 08 done

--- JAVA/module08/index.js
+++ JAVA/module08/index.js
@@ -46,3 +46,107 @@
       размер. Пусть маленькие изображения для preview будут 320px по ширине, большие для fullview 1280px.
       Подберите изображения одинаковых пропорций.
 */
+
+const galleryItems = [
+  {
+    preview: "./img/preview-1.jpg",
+    fullview: "./img/full-1.jpg",
+    alt: "image-1"
+  },
+  {
+    preview: "./img/preview-2.jpg",
+    fullview: "./img/full-2.jpg",
+    alt: "image-2"
+  },
+  {
+    preview: "./img/preview-3.jpg",
+    fullview: "./img/full-3.jpg",
+    alt: "image-3"
+  },
+  {
+    preview: "./img/preview-4.jpg",
+    fullview: "./img/full-4.jpg",
+    alt: "image-4"
+  },
+  {
+    preview: "./img/preview-5.jpg",
+    fullview: "./img/full-5.jpg",
+    alt: "image-5"
+  },
+  {
+    preview: "./img/preview-6.jpg",
+    fullview: "./img/full-6.jpg",
+    alt: "image-6"
+  }
+];
+
+const createFullviewItem = function(item) {
+  const fullview = document.createElement("div");
+  fullview.classList.add("fullview");
+
+  const img = document.createElement("img");
+  img.setAttribute("src", item.fullview);
+  img.setAttribute("alt", item.alt);
+
+  fullview.appendChild(img);
+  return fullview;
+};
+
+const createPreviewItem = function(item) {
+  const preview = document.createElement("li");
+  preview.classList.add("preview");
+
+  const img = document.createElement("img");
+  img.setAttribute("src", item.preview);
+  img.setAttribute("data-fullview", item.fullview);
+  img.setAttribute("alt", item.alt);
+
+  preview.appendChild(img);
+  return preview;
+};
+
+const createPreviewItems = function(items) {
+  const previewItems = items.map(item => createPreviewItem(item));
+  return previewItems;
+};
+
+const addItem = function() {
+  const imageGallery = document.querySelector(".image-gallery");
+  imageGallery.classList.add("js-image-gallery");
+
+  const fullviewItem = createFullviewItem(galleryItems[0]);
+  imageGallery.appendChild(fullviewItem);
+
+  const previewItemsList = document.createElement("ul");
+  previewItemsList.classList.add("preview-list");
+
+  createPreviewItems(galleryItems).forEach(element => {
+    previewItemsList.appendChild(element);
+  });
+
+  const firstItem = previewItemsList.firstElementChild.querySelector("img");
+  firstItem.classList.add("active");
+
+  imageGallery.appendChild(previewItemsList);
+};
+
+addItem();
+
+const gallery = document.querySelector(".js-image-gallery");
+gallery.addEventListener("click", handleNavClick);
+
+function handleNavClick(e) {
+  e.preventDefault();
+  const target = e.target;
+  if (!target.hasAttribute("data-fullview")) return;
+
+  const activeItem = gallery.querySelector("img.active");
+  if (activeItem) {
+    activeItem.classList.remove("active");
+  }
+  target.classList.add("active");
+
+  const fullviewImg = gallery.querySelector(".fullview img");
+  const attr = target.getAttribute("data-fullview");
+  fullviewImg.setAttribute("src", attr);
+}
